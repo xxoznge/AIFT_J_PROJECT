bec635be3e3c118d0b4f23656d16ac91ec7d3304
Sojung Lee <90453158+xxoznge@users.noreply.github.com>
2022-11-30 18:15:53 +0900

    log_slack_ㅅㅓㅇㄱㅗㅇ

--- config/slack.py
+++ config/slack.py
@@ -5,7 +5,7 @@ class MyMsg():
         response = requests.post(
             'https://slack.com/api/chat.postMessage',
             headers={
-                'Authorization': 'Bearer '+'토큰 복사하기'
+                'Authorization': 'Bearer '+'xoxb-4384492639233-4456863566545-JQMWJozEkTKEfGtg2rGI83WJ'
             },
             data={
                 'channel':'#realmessage',
@@ -13,3 +13,5 @@ class MyMsg():
             }
         )
         print(response)
+
+MyMsg().send_msg(msg="슬랙으로 전송하기")
